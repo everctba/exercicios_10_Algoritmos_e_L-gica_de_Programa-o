3f7b82cb6928293f5e3661d6fc766b5b76d0b6f1
everctba <everson777@gmail.com>
2024-04-10 22:21:29 -0300

    Update 06-exercicio.py
    Removido print do contador

--- 06-exercicio.py
+++ 06-exercicio.py
@@ -6,14 +6,15 @@ numero_digitado = 100
 numero_digitado = input("Digite seu número para ver em binário: ")
 while int(numero_digitado) >= 1:
    quociente = int(int(numero_digitado)/2)
-   print("Quociente: ", quociente)
+   #print("Quociente: ", quociente)
    resto = int(numero_digitado)%2
-   print("Resto: ", resto)
+   #print("Resto: ", resto)
    numero_digitado = quociente
    if resto == 0:
       numero_em_binario = str("0") + numero_em_binario
    else:
       numero_em_binario = str("1") + numero_em_binario
+print("--------------------")
 print("Seu Número em Binário é: ", numero_em_binario)   
    
    
